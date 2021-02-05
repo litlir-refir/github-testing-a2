52e4b8da833e6769a69aab35696b50da5f7f88e9
hycner <hycner@users.noreply.github.com>
2021-02-05 13:57:25 -0800

    updating secrets

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,13 +1,16 @@
 echo "ORG_GLOBAL: $ORG_GLOBAL"
 echo "ORG_A1: $ORG_A1"
 echo "ORG_A2: $ORG_A2"
+echo ""
 
 echo "REPO_A1_GENERAL: $REPO_A1_GENERAL"
 echo "REPO_A1_QA: $REPO_A1_QA"
 echo "REPO_A1_PROD: $REPO_A1_PROD"
+echo ""
 
 echo "REPO_A2_GENERAL: $REPO_A2_GENERAL"
 echo "REPO_A2_QA: $REPO_A2_QA"
 echo "REPO_A2_PROD: $REPO_A2_PROD"
+echo ""
 
 echo "test2"
